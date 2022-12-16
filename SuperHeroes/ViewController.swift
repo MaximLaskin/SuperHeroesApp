154fab50e70ffb12f946cec9fe5300e5abe41b10
MaximLaskin <maxlaskin@mail.ru>
2022-12-16 13:06:28 +0300

    Initial Commit

---
+++ SuperHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperHeroes
+//
+//  Created by Maxim on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
